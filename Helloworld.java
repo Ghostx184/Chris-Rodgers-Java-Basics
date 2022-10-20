3bf76a2f7b146bba6c313fbed2ef73d1d6158fd8
Ghostx184 <88863870+Ghostx184@users.noreply.github.com>
2022-10-20 13:37:22 -0500

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,17 @@
+
+// Boston tutorial 4
+// ChriRodgers tutorial 1
+
+//No class no Java
+
+public class Helloworld 
+{
+//Method is recipie on how to run your Program
+
+	//void beefcake() // Meethod header  baefcake is name of the method.
+
+	public static void main (String args[])// Method header
+	{
+		System.out.println("Hello Chris Rodgers");// print in the system
+	}
+}
