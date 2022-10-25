a076f95a835b85122e347bd332aa655382e17785
Ghostx184 <88863870+Ghostx184@users.noreply.github.com>
2022-10-24 19:03:36 -0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,33 @@
+package Tutorials;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int V = 5; //Declaring the variable and giving it a value
+		double f; //declaring just the variable
+		int z;
+		int y;
+		
+		
+	// does not know that z is an int	
+	//z = V;
+		f = 5; // naming the value later
+		y = 7;
+		//can not do int and variable
+		//z = f + V;
+	
+		z = V + y;// can do int + int
+	
+		//Printing out the results
+	System.out.print("Z is ");
+	System.out.print(z);
+	System.out.print(" ");
+	System.out.println(f); //Doubles have decimals like 5.0 even if you do not add them. 
+	System.out.println("Got it. Good Job");
+
+	
+	
+	}
+
+}
