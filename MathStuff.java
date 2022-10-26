b33e6ea493b02f03cac6c78bfecb83f4a4ba7f9a
Ghostx184 <88863870+Ghostx184@users.noreply.github.com>
2022-10-26 05:26:30 -0500

    Add files via upload

---
+++ MathStuff.java
@@ -0,0 +1,41 @@
+package Tutorials;
+
+public class MathStuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x = 12;
+		int y = 2;
+		int z;
+		
+		System.out.println("This is x");
+		System.out.println(x);
+		System.out.println("This is y");
+		System.out.println(y);
+		
+		
+		System.out.println("This is Add");
+		z = x +y;
+		System.out.println(z);
+		System.out.println("This is subtract");
+		z = x -y;
+		System.out.println(z);
+		System.out.println("This is Multiply");
+		z = x *y;
+		System.out.println(z);
+		System.out.println("This is Divide");
+		z = x /y;
+		System.out.println(z);
+		
+		//increment
+		System.out.println("This is x");
+		System.out.println(x);
+		x++;
+		System.out.println("This is x incrementally");
+		System.out.println(x);
+		
+	}
+
+}
